f70c913706336df0dbe524676c29aef9d7a719a5
clymerrm <clymerrm@gmail.com>
2020-01-07 13:09:23 -0500

    Adding locators to bottom of page

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -20,6 +20,15 @@ export class Footer extends Component {
                         if (window.confirm('Are you sure you wish to reset all of your todos?')) this.onClick(e)
                     }}>Reset Todos
                     </button>
+                    <button>
+                        <a style={linkStyle} href="https://saucelabs.com/resources/articles/selenium-tips-css-selectors" target="_blank">CSS Selectors</a>
+                    </button>
+                    <button>
+                        <a style={linkStyle} href="https://www.lambdatest.com/blog/complete-guide-for-using-xpath-in-selenium-with-examples/" target="_blank">Xpath Selectors</a>
+                    </button>
+                    <button>
+                        <a style={linkStyle} href="https://www.lambdatest.com/blog/locators-in-selenium-webdriver-with-examples/" target="_blank">General Selectors</a>
+                    </button>
                 </footer>
             )
         }
@@ -33,6 +42,12 @@ const headerStyle = {
     color: '#fff',
     textAlign: 'center',
     padding: '10px'
+};
+
+const linkStyle = {
+    color: '#fff',
+    textDecoration: 'none',
+    display: 'block'
 };
 
 export default Footer;
